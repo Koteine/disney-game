80c3ed90c47be856fcb23cf2331ac34bf75bbfc2
Koteine <tentalya@gmail.com>
2026-03-11 16:43:41 +0300

    Delete functions directory

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require('firebase-functions');
-
-// Telegram bot notifications were removed.
-// Kept as a minimal module so Firebase Functions deployment remains valid.
-exports.healthcheck = functions.https.onRequest((req, res) => {
-  res.status(200).send('ok');
-});
